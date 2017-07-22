aa317ea673e1f5ff250c9af7a38a99c6f12c69a7
Dai Mai <dai.mai@mediastep.com>
2017-07-22 15:27:45 +0700

    config proxy

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -8,6 +8,29 @@ const ENV = 'prod';
 
 module.exports = webpackMerge(commonConfig({ env: ENV }), {
     devtool: 'source-map',
+    devServer: {
+        contentBase: './target/www',
+        proxy: [{
+            context: [
+                /* jhipster-needle-add-entity-to-webpack - JHipster will add entity api paths here */
+                '/api',
+                '/management',
+                '/swagger-resources',
+                '/v2/api-docs',
+                '/h2-console'
+            ],
+            target: 'http://192.168.0.107:8080',
+            secure: false,
+            changeOrigin: true,
+            pathRewrite: {
+                '/api': '/quanlyso/api',
+                '/management': '/quanlyso/management',
+                '/swagger-resources': '/quanlyso/swagger-resources',
+                '/v2/api-docs': '/quanlyso/v2/api-docs',
+                '/h2-console': '/quanlyso/h2-console'
+            }
+        }]
+    },
     output: {
         path: path.resolve('./target/www'),
         filename: '[hash].[name].bundle.js',
